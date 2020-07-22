b7135ac6ab40fffd534239875ed978e5b476fb60
Khris Punzalan <khrispunzalan@gmail.com>
2020-07-22 18:36:22 -0500

    added Main & Note Components

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Main from './app/components/Main';
+import Note from './app/components/Note';
+
+export default class baseToDo extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style='auto' />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
