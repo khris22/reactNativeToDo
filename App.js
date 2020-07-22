df999c40f603ea358338a414500a71187e156dcf
Khris Punzalan <khrispunzalan@gmail.com>
2020-07-22 18:44:36 -0500

    imported Main Components to App.js

--- App.js
+++ App.js
@@ -1,26 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
 import Main from './app/components/Main';
 import Note from './app/components/Note';
 
 export default class baseToDo extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style='auto' />
-      </View>
-    );
+    return <Main />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- app/components/Main.js
+++ app/components/Main.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 
 export class Main extends Component {
   render() {
-    return <div></div>;
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    //   flex: 1,
+    //   backgroundColor: '#fff',
+    //   alignItems: 'center',
+    //   justifyContent: 'center',
+  },
+});
 
 export default Main;
