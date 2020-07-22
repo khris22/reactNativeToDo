5c328fc7343ecab5ce5b889f3e22e5ed7af77518
Khris Punzalan <khrispunzalan@gmail.com>
2020-07-22 18:57:18 -0500

    added ios simulator

--- app/components/Main.js
+++ app/components/Main.js
@@ -12,7 +12,7 @@ export class Main extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>TO DO APP</Text>
       </View>
     );
   }
