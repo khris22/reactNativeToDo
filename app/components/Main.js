e86055c2c7afb5a0b33c392789d191d01fd3fcf5
Khris Punzalan <khrispunzalan@gmail.com>
2020-07-23 18:59:35 -0500

    added styling  using Stylesheet

--- app/components/Main.js
+++ app/components/Main.js
@@ -23,7 +23,7 @@ export class Main extends Component {
         {/* FOOTER */}
         <View style={styles.footer}>
           <TextInput
-            style={style.textInput}
+            style={styles.textInput}
             placeholder='ADD TASK HERE'
             placeholderTextColor='white'
             underlineColorAndroid='transparent'></TextInput>
@@ -35,10 +35,58 @@ export class Main extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    //   flex: 1,
-    //   backgroundColor: '#fff',
-    //   alignItems: 'center',
-    //   justifyContent: 'center',
+    flex: 1,
+    backgroundColor: '#a8e6cf',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: '#dcedc1',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomWidth: 10,
+    borderBottomColor: '#ffd3b6',
+  },
+  hText: {
+    color: 'white',
+    fontSize: 20,
+    padding: 26,
+  },
+  scrollContainer: {
+    flex: 1,
+    marginBottom: 100,
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 10,
+  },
+  textInput: {
+    alignSelf: 'stretch',
+    color: '#fff',
+    padding: 20,
+    backgroundColor: '#ff8b94',
+    borderTopWidth: 2,
+    borderTopColor: '#ffaaa5',
+  },
+  addButton: {
+    position: 'absolute',
+    zIndex: 11,
+    right: 20,
+    bottom: 90,
+    backgroundColor: '#29a8ab',
+    width: 90,
+    height: 90,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 8,
+  },
+  addButtonText: {
+    color: '#5e5656',
+    fontSize: 24,
   },
 });
 
