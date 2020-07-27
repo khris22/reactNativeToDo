ca31d4f7a32a76d6f41e10a74de1acb2b97930e5
Khris Punzalan <khrispunzalan@gmail.com>
2020-07-26 21:03:20 -0500

    added css on note

--- app/components/Note.js
+++ app/components/Note.js
@@ -19,4 +19,30 @@ export class Note extends Component {
 
 export default Note;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  note: {
+    position: 'relative',
+    padding: 20,
+    paddingRight: 100,
+    borderBottomWidth: 2,
+    borderBottomColor: '#ededed',
+  },
+  noteText: {
+    paddingLeft: 20,
+    borderLeftWidth: 10,
+    borderLeftColor: '#e91e63',
+  },
+  noteDelete: {
+    position: 'absolute',
+    justify: 'center',
+    backgroundColor: '#2980b9',
+    padding: 10,
+    top: 10,
+    top: 10,
+    bottom: 10,
+    right: 10,
+  },
+  noteDeleteText: {
+    color: 'white',
+  },
+});
